9d892a317d17bf88aa6fb959beca4c1297e9f91e
MikeLin <linmichael97@yahoo.com>
2019-06-06 15:42:41 -0400

    Initial Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pokemon_project.wsgi --log-file - 
---
+++ settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE pokemon_database;
+CREATE USER pokemonuser WITH PASSWORD 'pokemon';
+GRANT ALL PRIVILEGES ON DATABASE pokemon_database TO pokemonuser; 
