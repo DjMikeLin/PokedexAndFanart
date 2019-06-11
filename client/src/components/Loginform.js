58a99ac40defbdb9f986ff1d487b3813c96fff66
MikeLin <linmichael97@yahoo.com>
2019-06-11 10:26:16 -0400

    Added route and component for user login

--- client/src/components/Loginform.js
+++ client/src/components/Loginform.js
@@ -1,23 +1,56 @@
 import React from 'react';
+import {findUser} from './axiosRouter';
 
 class Loginform extends React.Component {
     state = {
-        showLogin: true
+        name: '',
+        password: '',
+        showLogin: true,
+        errorMssg: '',
+        fanarts: [],
+        favorites: []
     }
 
-    submitLogin = e => {
+    submitLogin = async(e) => {
         e.preventDefault();
-        this.setState({showLogin: false});
+
+        let result = (await findUser(this.state.name)).data[0];
+        //if username is not found or password does not match 
+        if(result === undefined || this.state.password !== result.password){
+            this.setState({errorMssg: 'Wrong Username or Password!'});
+            return;
+        }
+        this.setState({showLogin: false, fanarts: result.fanarts, favorites: result.favorites});
+    }
+
+    handleChange = e => {
+        this.setState({errorMssg: ''});
+        switch(e.target.name){
+            case 'name':
+                this.setState({name: e.target.value});
+                break;
+            case 'password':
+                this.setState({password: e.target.value});
+                break;
+            default:
+                break; 
+        }
     }
 
     render(){
         return(
+            <div>{
             this.state.showLogin?
             <form onSubmit={this.submitLogin}>
-                <input type="text" name="name"/>
-                <input type="password" name="password"/>
-                <input type="submit"/>
-            </form>:null
+                <label>Username: </label>
+                <input type="text" name="name" onChange={this.handleChange}/>
+                <label>Password: </label>
+                <input type="password" name="password" onChange={this.handleChange}/>
+                <button type="submit">Login</button>
+            </form>:null}
+
+            <p>{this.state.errorMssg}</p>
+            </div>
         )
     }
 }
--- client/src/components/axiosRouter.js
+++ client/src/components/axiosRouter.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-export async function 
+export async function findUser(userName){
+    return await axios.get('/api/v1/users/' + userName);
+}
 
 export async function test(){
     return await axios.get('/api/v1/pokedex/60');
